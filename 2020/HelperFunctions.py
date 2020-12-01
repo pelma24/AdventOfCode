13c3bbb8c2d3af1e84a687ab14500756b9262a39
Iris Kellermann <Iris.Kellermann@methodpark.de>
2020-12-01 08:58:21 +0100

    Make it more beautiful

---
+++ 2020/HelperFunctions.py
@@ -0,0 +1,9 @@
+def inputsplit(input):
+    return input.split()
+
+def convertToInt(input):
+    numbers = []
+    for stringNumber in input:
+        numbers.append(int(stringNumber))
+
+    return numbers
