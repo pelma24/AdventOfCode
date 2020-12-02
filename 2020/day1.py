3b7b1f5fe4d3a92ae1eb5a8df9ecadee9055c92f
Iris Kellermann <Iris.Kellermann@methodpark.de>
2020-12-02 09:18:48 +0100

    Remove blank line and adapt to helper function

--- 2020/day1.py
+++ 2020/day1.py
@@ -4,7 +4,6 @@ from HelperFunctions import convertToInt
 def do1(intInput):
     
     multiply = set([x*y for x in intInput for y in intInput if (x + y) == 2020])
-
     return multiply
 
 def do2(intInput):
@@ -12,12 +11,11 @@ def do2(intInput):
     multiply = set([x*y*z for x in intInput for y in intInput for z in intInput if (x + y + z) == 2020])
     return multiply
 
-
 def do():
     with open ('Input/day1.txt') as f:
         strInput = f.read()
 
-    splitInput = inputsplit(strInput)
+    splitInput = inputsplit(strInput, '\n')
     intInput = convertToInt(splitInput)
 
     print(do1(intInput))
