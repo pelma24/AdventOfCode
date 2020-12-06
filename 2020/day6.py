49a58575e30f019933eb12b96ea20d55166f8e41
Iris Kellermann <Iris.Kellermann@methodpark.de>
2020-12-06 09:36:39 +0100

    Remove unnecessary function

--- 2020/day6.py
+++ 2020/day6.py
@@ -5,12 +5,12 @@ def do1(puzzleInput):
     groupCount = 0
 
     for group in puzzleInput:
-        answers = []
+        answers = set()
         persons = group.split('\n')
         for person in persons:
-            for answer in person:
-                answers.append(answer)
-        groupCount += len(set(answers))
+            answer = set(person)
+            answers = answers.union(answer)
+        groupCount += len(answers)
 
     return groupCount
 
@@ -20,21 +20,14 @@ def do2(puzzleInput):
 
     for group in puzzleInput:
         persons = group.split('\n')
-        sameAnswers = getAnswers(persons[0])
+        sameAnswers = set(persons[0])
         for otherPerson in persons[1:]:
-            answer = getAnswers(otherPerson)
+            answer = set(otherPerson)
             sameAnswers = sameAnswers.intersection(answer)        
 
         groupCount += len(sameAnswers)
 
     return groupCount
-
-def getAnswers(person):
-    answers = []
-    for answer in person:
-        answers.append(answer)
-    
-    return set(answers)
 
 def do():
     with open ('Input/day6.txt') as f:
