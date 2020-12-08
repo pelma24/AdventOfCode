2720fb563241870687e97b060571335fef0df7bb
Iris Kellermann <Iris.Kellermann@methodpark.de>
2020-12-08 07:35:10 +0100

    Refactor

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,12 +1,41 @@
 from HelperFunctions import inputsplit
 
 def do1(puzzleInput):
+    success, acc = execute(puzzleInput)
+    
+    if not success:
+        return acc
+    else:
+        return 'error'
+
+def do2(oldPuzzleInput):
+    replacePosition = -1
+    while (replacePosition < len(oldPuzzleInput)):
+        replacePosition += 1
+        puzzleInput = oldPuzzleInput.copy()
+        
+        if not replaceInstruction(replacePosition, puzzleInput):
+            continue
+        
+        success, acc = execute(puzzleInput)
+
+        if success:
+            return acc
+        
+    return 'error'    
+
+def execute(puzzleInput):
     acc = 0
 
     visited = [0 for x in range(len(puzzleInput))]
 
     position = 0
-    while (visited[position] != 1):
+    while True:
+        if position == len(puzzleInput):
+            return [True, acc]
+        if visited[position] == 1:
+            return [False, acc]
+    
         instructions = puzzleInput[position].split(' ')
 
         if instructions[0] == 'nop':
@@ -22,46 +51,6 @@ def do1(puzzleInput):
             visited[position] = 1
             position += int(instructions[1])
             continue
-
-    return acc
-
-def do2(oldPuzzleInput):
-    replacePosition = -1
-    while (replacePosition < len(oldPuzzleInput)):
-        replacePosition += 1
-        puzzleInput = oldPuzzleInput.copy()
-        
-        if not replaceInstruction(replacePosition, puzzleInput):
-            continue
-
-        acc = 0
-
-        visited = [0 for x in range(len(puzzleInput))]
-
-        position = 0
-        while True:
-            if position == len(puzzleInput):
-                return acc
-            if visited[position] == 1:
-                break
-        
-            instructions = puzzleInput[position].split(' ')
-
-            if instructions[0] == 'nop':
-                visited[position] = 1
-                position += 1
-                continue
-            if instructions[0] == 'acc':
-                visited[position] = 1
-                acc += int(instructions[1])
-                position += 1
-                continue
-            if instructions[0] == 'jmp':
-                visited[position] = 1
-                position += int(instructions[1])
-                continue
-
-    return 'error'    
 
 def replaceInstruction(position, puzzleInput):
     if 'acc' in puzzleInput[position]:
