5b86b61c3e3ab6cfb87ffead11e3cd9c6abd0d83
Iris Kellermann <iris.kellermann@methodpark.de>
2020-12-18 08:30:25 +0100

    day18

---
+++ 2020/day18.py
@@ -0,0 +1,71 @@
+from HelperFunctions import inputsplit
+import re
+
+def do1(puzzleInput):
+    sum = 0
+    for line in puzzleInput:
+        sum += calculate(line, sumUp)
+
+    return sum
+
+def do2(puzzleInput):
+    sum = 0
+    for line in puzzleInput:
+        lineResult = calculate(line, sumWithLineBeforeDot)
+        sum += lineResult
+
+    return sum
+
+def calculate(line, sumFunction):
+    matches = re.findall('\([0-9 \+\*]+\)', line)
+    while matches:
+        match = matches[0]
+        result = sumFunction(match[1:-1])
+        line = line.replace(match, str(result), 1)
+        matches = re.findall('\([0-9 \+\*]+\)', line)
+    
+    result = sumFunction(line)
+            
+    return result
+
+def sumWithLineBeforeDot(line):
+    matches = re.findall('[0-9]+ \+ [0-9]+', line)
+    while matches:
+        match = matches[0]
+        result = sumUp(match)
+        line = line.replace(match, str(result), 1)
+        matches = re.findall('[0-9]+ \+ [0-9]+', line)
+    return sumUp(line)
+
+def sumUp(line):
+    result = 0
+    operation = add
+    for symbol in line.split(' '):
+        if symbol.isnumeric():
+            symbol = int(symbol)
+            result = operation(result, symbol)
+            continue
+        elif symbol == '+':
+            operation = add
+            continue
+        elif symbol == '*':
+            operation = multiply
+            continue
+    return result
+
+def multiply(result, symbol):
+    return result * symbol
+
+def add(result, symbol):
+    return result + symbol
+
+def do():
+    with open ('Input/day18.txt') as f:
+        strInput = f.read()
+
+    splitInput = inputsplit(strInput, '\n')
+
+    print(do1(splitInput))
+    print(do2(splitInput))
+    
+do()
