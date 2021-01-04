c66d72fa0db6064fed001d95eea4b2f2b45c9a68
Iris Kellermann <iris.kellermann@methodpark.de>
2021-01-04 15:19:45 +0100

    Alternative solution for day10 (that I do understand)

--- 2020/day10.py
+++ 2020/day10.py
@@ -2,40 +2,41 @@ from HelperFunctions import inputsplit
 from HelperFunctions import convertToInt
 
 def do1(puzzleInput):
+    prepareInput(puzzleInput)
+    
     differences = getDifferences(puzzleInput)
 
     return (differences.count(1)) * (differences.count(3))
 
-def getDifferences(oldPuzzleInput):
-    puzzleInput = oldPuzzleInput.copy()
+def do2(puzzleInput):
+    prepareInput(puzzleInput)
+    
+    return getNumberOfPossibilities(puzzleInput)
+
+def prepareInput(puzzleInput):
     puzzleInput.insert(0,0)
     puzzleInput.insert(-1, max(puzzleInput) + 3)
     puzzleInput.sort()
-    
+
+def getDifferences(puzzleInput):
+        
     differences = [puzzleInput[x+1] - puzzleInput[x] for x in range(len(puzzleInput)-1)]
     
     return differences
+
+def getNumberOfPossibilities(puzzleInput):
     
+    puzzleInput.reverse()
 
-def do2(puzzleInput):
-    differences = getDifferences(puzzleInput)
-    possibilities = 0
-    sequences = []
+    possibilites = {puzzleInput[0]:1}
 
-    sequence = 0
-    for difference in differences:
-        if difference == 3:
-            if sequence:
-                sequences.append(sequence)
-            sequence = 0
-        else:
-            sequence += 1
-
-    dic = {1: lambda x : x, 2: lambda x : x * 2, 3: lambda x: x * 4, 4: lambda x : x * 7}
-    possibilities = 1
-    for sequence in sequences:
-        possibilities = dic[sequence](possibilities)
-    return possibilities
+    for value in puzzleInput[1:]:
+        sum = 0
+        for i in [1,2,3]:
+            sum += possibilites.get(value + i, 0)
+        possibilites[value] = sum
+    
+    return possibilites[puzzleInput[-1]] 
 
 def do():
     with open ('Input/day10.txt') as f:
