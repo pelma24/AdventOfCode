dbb986fec2d87c1b5e5965d3d05f5b2dd99d310a
Iris Kellermann <iris.kellermann@methodpark.de>
2021-11-05 07:45:04 +0100

    Prepare for AoC 2021

---
+++ 2021/HelperFunctions.py
@@ -0,0 +1,7 @@
+
+def convertToInt(input):
+    numbers = []
+    for stringNumber in input:
+        numbers.append(int(stringNumber))
+
+    return numbers
---
+++ 2021/day1.py
@@ -0,0 +1,20 @@
+from HelperFunctions import convertToInt
+
+def do1(splitInput):
+    print('done')
+
+def do2(splitInput):
+    print('done')
+
+def do():
+    with open ('Input/day1.txt') as f:
+        strInput = f.read()
+
+    do1(strInput)
+    do2(strInput)
+
+    print('done')
+    
+
+do()
+
