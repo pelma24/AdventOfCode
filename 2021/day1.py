18ff567473532200c23067b9f560fa7494fdb67a
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-01 08:06:35 +0100

    Solve day1

--- 2021/day1.py
+++ 2021/day1.py
@@ -2,19 +2,26 @@ from HelperFunctions import readInputFile
 from HelperFunctions import convertToInt
 
 def do1(splitInput):
-    print('done')
+    
+    bigger = [(x,y) for (x,y) in zip(splitInput, splitInput[1:]) if y > x]
+
+    return len(bigger)
 
 def do2(splitInput):
-    print('done')
+    window1 = [a + b + c for (a,b,c) in zip(splitInput, splitInput[1:], splitInput[2:])]
+    window2 = [b + c + d for (b,c,d) in zip(splitInput[1:], splitInput[2:], splitInput[3:])]
+
+    bigger = [(x,y) for (x,y) in zip(window1, window2) if y > x]
+
+    return len(bigger)
 
 def do():
     strInput = readInputFile(1)
 
-    do1(strInput)
-    do2(strInput)
-
-    print('done')
-    
+    splitInput = strInput.split('\n')
+    intInput = convertToInt(splitInput)
+    print(do1(intInput))
+    print(do2(intInput))
 
 do()
 
