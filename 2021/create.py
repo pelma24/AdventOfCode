9ed69e87162ddb13baba41ed991d6ca28ba91343
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-01 08:08:21 +0100

    Minor change in day creation

--- 2021/create.py
+++ 2021/create.py
@@ -6,8 +6,8 @@ def createFiles(filepath, filepathInput, day):
     if not Path(filepath).exists():
         with open(filepath, 'w') as f:
             f.writelines(['from HelperFunctions import readInputFile\n', 'from HelperFunctions import convertToInt\n\n', 'def do1(splitInput):\n', 
-            '\tprint(\'done\')\n\n', 'def do2(splitInput):\n', '\tprint(\'done\')\n\n', 'def do():\n', f'\tstrInput = readInputFile({day})\n\n', 
-            '\tdo1(strInput)\n', '\tdo2(strInput)\n\n', '\tprint(\'done\')\n\n\n', 'do()'])
+            '\treturn \'done\'\n\n', 'def do2(splitInput):\n', '\treturn \'done\'\n\n', 'def do():\n', f'\tstrInput = readInputFile({day})\n\n', 
+            '\tprint(do1(strInput))\n', '\tprint(do2(strInput))\n\n', '\tprint(\'done\')\n\n\n', 'do()'])
     if not Path(filepathInput).exists():
         open(filepathInput, 'a').close()
 
