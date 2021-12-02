488ba52af9c0ee804f998721982caf8e988738c0
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-02 07:56:15 +0100

    Solution for day2

--- 2021/day2.py
+++ 2021/day2.py
@@ -3,16 +3,48 @@ from HelperFunctions import readExampleInput
 from HelperFunctions import convertToInt
 
 def do1(splitInput):
-	return 'done'
+	horizontal = 0
+	depth = 0
+
+	for line in splitInput:
+		direction,value = line.split(' ')
+
+		match direction:
+			case 'forward':
+				horizontal += int(value)
+			case 'up':
+				depth -= int(value)
+			case 'down':
+				depth += int(value)
+	
+	return horizontal * depth
 
 def do2(splitInput):
-	return 'done'
+	horizontal = 0
+	depth = 0
+	aim = 0
+	
+	for line in splitInput:
+		direction,value = line.split(' ')
+
+		match direction:
+			case 'forward':
+				horizontal += int(value)
+				depth += aim * int(value)
+			case 'up':
+				aim -= int(value)
+			case 'down':
+				aim += int(value)
+	
+	return horizontal * depth
 
 def do():
 	strInput = readInputFile(2)
 
-	print(do1(strInput))
-	print(do2(strInput))
+	splitInput = strInput.split('\n')
+
+	print(do1(splitInput))
+	print(do2(splitInput))
 
 	print('done')
 
