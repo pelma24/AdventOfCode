b41c07c6c13e71cd17a2c40ca8f7dbe37ff8a320
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-03 08:58:49 +0100

    Preparation for day4

---
+++ 2021/day4.py
@@ -0,0 +1,20 @@
+from HelperFunctions import readInputFile
+from HelperFunctions import readExampleInput
+from HelperFunctions import convertToInt
+
+def do1(splitInput):
+	return 'done'
+
+def do2(splitInput):
+	return 'done'
+
+def do():
+	strInput = readInputFile(4)
+
+	print(do1(strInput))
+	print(do2(strInput))
+
+	print('done')
+
+
+do()
