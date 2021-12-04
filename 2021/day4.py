04e1daa65b6a83b9a70c579dbe17c17f6e28291c
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-04 06:40:00 +0100

    Part1 of day4

--- 2021/day4.py
+++ 2021/day4.py
@@ -1,18 +1,79 @@
 from HelperFunctions import readInputFile
 from HelperFunctions import readExampleInput
-from HelperFunctions import convertToInt
 
 def do1(splitInput):
-	return 'done'
+	numbers = convertToInt(splitInput[0].split(','))
+
+	boards = splitInput[1:]
+	intBoards = []
+	for board in boards:
+		splitBoard = board.split('\n')
+		intSplitBoard = []
+		for line in splitBoard:
+			intSplitBoard.append(convertToInt(line.split(' ')))
+		intBoards.append(intSplitBoard)
+
+	return play(numbers, intBoards)		
+	
 
 def do2(splitInput):
 	return 'done'
 
+def convertToInt(input):
+	numbers = []
+	for stringNumber in input:
+		if stringNumber == '' :
+			continue
+		else:
+			numbers.append(int(stringNumber))
+	return numbers
+
+def play(numbers, boards):
+	for number in numbers:
+		for board in boards:
+			playNumberOnBoard(board, number)
+			winner = winningBoard(boards)
+		if  winner != -1:
+			return number * winningScore(boards[winner])
+
+def playNumberOnBoard(board, number):
+	for line in board:
+		if number in line:
+			index = line.index(number)
+			line[index] = 'X'			
+
+def winningBoard(boards):
+	winning = -1
+	for number,board in enumerate(boards):
+		if ['X', 'X', 'X', 'X', 'X'] in board:
+			winning = number
+		else:
+			for i in range(len(board[0])):
+				completeLine = True
+				for j in range(len(board)):
+					if board[j][i] != 'X':
+						completeLine = False
+						break
+				if completeLine:
+					winning = number
+					break
+	return winning
+
+def winningScore(board):
+	winningSum = 0
+	for line in board:
+		winningSum += sum([x for x in line if x != 'X'])
+
+	return winningSum
+
 def do():
 	strInput = readInputFile(4)
+	#strInput = readExampleInput(4)
 
-	print(do1(strInput))
-	print(do2(strInput))
+	splitInput = strInput.split('\n\n')
+
+	print(do1(splitInput))
+	print(do2(splitInput))
 
 	print('done')
 
