00bda9ce5bd0b3d18697e760e910c654bcea7e3c
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-07 08:46:08 +0100

    Small optimizations

--- 2021/day7.py
+++ 2021/day7.py
@@ -6,29 +6,29 @@ def do1(crabs):
 	fuelSum = 0
 	minFuelSum = 10000000
 
-	for i in range(10000):
+	for i in range(min(crabs), max(crabs) + 1):
 		fuelSum = 0
 		for crab in crabs:
 			fuel = abs(i - crab)
 			fuelSum += fuel
-		if fuelSum < minFuelSum:
-			minFuelSum = fuelSum
+		minFuelSum = min(minFuelSum, fuelSum)
 	
 	return minFuelSum
 
 def do2(crabs):
-	steps = [i for i in range(1,10000)]
-	fuelNeeded = [sum(steps[0:i]) for i in range(1,10000)]
+	maxCrabs = max(crabs) + 1
+	crabRange = range(min(crabs), maxCrabs)
+	steps = [i for i in range(1, maxCrabs)]
+	fuelNeeded = [sum(steps[0:i]) for i in range(1, maxCrabs)]
 	minFuelSum = 10000000000
 
-	for i in range(10000):
+	for i in crabRange:
 		fuelSum = 0
 		for crab in crabs:
 			stepsNeeded = abs(i - crab)
 			fuel = fuelNeeded[stepsNeeded - 1]
 			fuelSum += fuel
-		if fuelSum < minFuelSum:
-			minFuelSum = fuelSum
+		minFuelSum = min(minFuelSum, fuelSum)
 	
 	return minFuelSum
 	
