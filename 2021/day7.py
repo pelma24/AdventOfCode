38bc23ff66bdf80e0a9380443f57ffa6fc969a27
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-07 06:39:43 +0100

    Day7 part2

--- 2021/day7.py
+++ 2021/day7.py
@@ -16,12 +16,24 @@ def do1(crabs):
 	
 	return minFuelSum
 
-def do2(splitInput):
-	return 'done'
+def do2(crabs):
+	steps = [i for i in range(1,10000)]
+	fuelNeeded = [sum(steps[0:i]) for i in range(1,10000)]
+	minFuelSum = 10000000000
 
+	for i in range(10000):
+		fuelSum = 0
+		for crab in crabs:
+			stepsNeeded = abs(i - crab)
+			fuel = fuelNeeded[stepsNeeded - 1]
+			fuelSum += fuel
+		if fuelSum < minFuelSum:
+			minFuelSum = fuelSum
+	
+	return minFuelSum
+	
 def do():
 	strInput = readInputFile(7)
-	#strInput = readExampleInput(7)
 
 	splitInput = strInput.split(',')
 	intInput = convertToInt(splitInput)
