38956898c5849d5109dcd32ad875b4ef0118e1f0
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-08 06:18:56 +0100

    Day8 part1

--- 2021/day8.py
+++ 2021/day8.py
@@ -3,16 +3,31 @@ from HelperFunctions import readExampleInput
 from HelperFunctions import convertToInt
 
 def do1(splitInput):
-	return 'done'
+	uniqueDigits = 0
+
+	for line in splitInput:
+		input,output = line.split('|')
+
+		outputDigits = output.split(' ')
+
+		for digit in outputDigits:
+			if len(digit) in [2, 3, 4, 7]:
+				uniqueDigits += 1
+	
+	return uniqueDigits
 
 def do2(splitInput):
+	
 	return 'done'
 
 def do():
 	strInput = readInputFile(8)
+	strInput = readExampleInput(8)
 
-	print(do1(strInput))
-	print(do2(strInput))
+	splitInput = strInput.split('\n')
+
+	print(do1(splitInput))
+	print(do2(splitInput))
 
 	print('done')
 
