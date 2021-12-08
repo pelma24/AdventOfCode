8d50a84f10a75a1e4a582b5575c71b28215cf5bd
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-08 09:07:14 +0100

    Day8 part2

--- 2021/day8.py
+++ 2021/day8.py
@@ -1,6 +1,11 @@
 from HelperFunctions import readInputFile
 from HelperFunctions import readExampleInput
 from HelperFunctions import convertToInt
+from collections import defaultdict
+
+numberMapping = {1: ['c', 'f'], 2: ['a', 'c', 'd', 'e', 'g'], 3: ['a', 'c', 'd', 'f', 'g'], 4: ['b', 'c', 'd', 'f'], 5: ['a', 'b', 'd', 'f', 'g'], 6: ['a', 'b', 'd', 'e', 'f', 'g'], 7: ['a', 'c', 'f'], 8: ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 9: ['a', 'b', 'c', 'd', 'f', 'g'], 0: ['a', 'b', 'c', 'e', 'f', 'g']}
+sameDigitsPerNumber = {2: ['c', 'f'], 3: ['a', 'c', 'f'], 4: ['b', 'c', 'd', 'f'], 7: ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 5: ['a', 'd', 'f'], 6: ['a', 'b', 'f', 'g']}
+allPossibleDigitsPerNumber = {2: ['c', 'f'], 3: ['a', 'c', 'f'], 4: ['b', 'c', 'd', 'f'], 7: ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 5: ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 6: ['a', 'b', 'c', 'd', 'e', 'f', 'g']}
 
 def do1(splitInput):
 	uniqueDigits = 0
@@ -17,12 +22,112 @@ def do1(splitInput):
 	return uniqueDigits
 
 def do2(splitInput):
+	sumOutputValues = 0
+
+	for line in splitInput:
+		input,output = line.split('|')
+
+		inputValues = input.split(' ')
+		outputValues = output.split(' ')
+
+		mapping = getMapping(inputValues)
+		outputValue = int(calculateOutputValues(outputValues, mapping))
+
+		sumOutputValues += outputValue
 	
-	return 'done'
+	return sumOutputValues
+
+def getMapping(inputValues):
+	mapping = {'a': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'b': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'c': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'd': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'e': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'f': set(('a', 'b', 'c', 'd', 'e', 'f', 'g')), 'g': set(('a', 'b', 'c', 'd', 'e', 'f', 'g'))}
+
+	inputPerLength = defaultdict(lambda: [])
+
+	for inputValue in inputValues[0:-1]:
+		length = len(inputValue)
+		inputPerLength[length].append([x for x in inputValue])
+		possibleValues = set(allPossibleDigitsPerNumber[length])
+		for digit in inputValue:
+			mapping[digit].intersection_update(possibleValues)
+	# a (7)
+	a = list(set(inputPerLength[3][0]).symmetric_difference(set(inputPerLength[2][0])))[0]
+	mapping[a] = {'a'}
+	for key in mapping.keys():
+		if key != a:
+			if 'a' in mapping[key]:
+				mapping[key].remove('a')
+
+	# c & f (1)
+	one = list(inputPerLength[2][0][0])[0]
+	otherOne = list(inputPerLength[2][0][1])[0]
+	sumOfOne = 0
+	for input in inputValues:
+		if one in input:
+			sumOfOne += 1
+	if sumOfOne == 8:
+		mapping[one] = {'c'}
+		removeFromMapping(one, 'c', mapping)
+		removeFromMapping(otherOne, 'f', mapping)
+	else:
+		mapping[one] = {'f'}
+		removeFromMapping(one, 'f', mapping)
+		removeFromMapping(otherOne, 'c', mapping)
+
+	# b & d (4)
+	four = [x for x in mapping.keys() if mapping[x] == {'b', 'd'}]
+	sumOfFour = 0
+	for input in inputValues:
+		if four[0] in input:
+			sumOfFour += 1
+	if sumOfFour == 6:
+		mapping[four[0]] = {'b'}
+		removeFromMapping(four[0], 'b', mapping)
+		removeFromMapping(four[1], 'd', mapping)
+	else:
+		mapping[four[0]] = {'d'}
+		removeFromMapping(four[0], 'd', mapping)
+		removeFromMapping(four[1], 'b', mapping)
+
+	# e & g
+	others = [x for x in mapping.keys() if mapping[x] == {'e', 'g'}]
+	sumOfOthers = 0
+	for input in inputValues:
+		if others[0] in input:
+			sumOfOthers += 1
+	if sumOfOthers == 4:
+		mapping[others[0]] = {'e'}
+		removeFromMapping(others[0], 'e', mapping)
+	else:
+		mapping[others[0]] = {'g'}
+		removeFromMapping(others[0], 'g', mapping)
+	
+	return mapping
+
+def removeFromMapping(rightKey, digit, mapping):
+	for key in mapping.keys():
+			if key != rightKey:
+				if digit in mapping[key]:
+					mapping[key].remove(digit)
+
+def calculateOutputValues(outputValues, mapping):
+	result = ''
+	for outputValue in outputValues:
+		if outputValue == '':
+			continue
+		
+		resultStr = []
+		for digit in outputValue:
+			resultStr.append(list(mapping[digit])[0])
+		
+		sortedResult = sorted(resultStr)
+
+		resultDigit = str([x for x in numberMapping.keys() if sorted(numberMapping[x]) == sortedResult][0])
+		result += resultDigit	
+	
+	return result
 
 def do():
 	strInput = readInputFile(8)
-	strInput = readExampleInput(8)
+	#strInput = readExampleInput(8)
 
 	splitInput = strInput.split('\n')
 
