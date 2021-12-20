67c10af9c8b8c3794a653232741921653cbad368
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-20 13:38:50 +0100

    Refactor

--- 2021/day20.py
+++ 2021/day20.py
@@ -6,44 +6,35 @@ from copy import deepcopy
 def do1(enhancement,image):
 	extendedImage = prepareImage(image, 10)
 	
-	newImage = deepcopy(extendedImage)
-
-	for i in range(2):
-		newImage = enhanceImage(newImage, enhancement)
-		newNewImage = []
-		for lineNumber,line in enumerate(newImage):
-			if lineNumber == 0 or lineNumber == len(newImage) - 1:
-				continue
-			newNewImage.append(line[1:-1])
-		newImage = deepcopy(newNewImage)
-			
-	
-	litPixels = 0
-	for line in newImage:
-		litPixels += len([x for x in line if x == '#'])
-
-	return litPixels
+	newImage = imageEnhancement(extendedImage, enhancement, 2)
+				
+	return getNumberOfLitPixels(newImage)
 
 def do2(enhancement,image):
 	extendedImage = prepareImage(image, 100)
 	
-	newImage = deepcopy(extendedImage)
+	newImage = imageEnhancement(extendedImage, enhancement, 50)
+				
+	return getNumberOfLitPixels(newImage)
 
-	for i in range(50):
-		newImage = enhanceImage(newImage, enhancement)
-		newNewImage = []
-		for lineNumber,line in enumerate(newImage):
-			if lineNumber == 0 or lineNumber == len(newImage) - 1:
-				continue
-			newNewImage.append(line[1:-1])
-		newImage = deepcopy(newNewImage)
-			
-	
+def getNumberOfLitPixels(image):
 	litPixels = 0
-	for line in newImage:
+	for line in image:
 		litPixels += len([x for x in line if x == '#'])
 
 	return litPixels
+
+def imageEnhancement(image, enhancement, repetitions):
+	for _ in range(repetitions):
+		image = enhanceImage(image, enhancement)
+		newImage = []
+		for lineNumber,line in enumerate(image):
+			if lineNumber == 0 or lineNumber == len(image) - 1:
+				continue
+			newImage.append(line[1:-1])
+		image = deepcopy(newImage)
+	
+	return image
 
 def enhanceImage(image, enhancement):
 	newImage = deepcopy(image)
@@ -51,7 +42,7 @@ def enhanceImage(image, enhancement):
 	for lineNumber,line in enumerate(image):
 		if lineNumber == 0 or lineNumber == len(image) - 1:
 			continue
-		for position,pixel in enumerate(line):
+		for position,_ in enumerate(line):
 			if position == 0 or position == len(line) - 1:
 				continue
 		
@@ -78,19 +69,15 @@ def getNeighbors(image, lineNumber, position):
 
 def prepareImage(image, additionSize):
 	newImage = []
-
 	emptyLine = ['.' for x in range(additionSize)] + ['.' for x in range(len(image[0]))] + ['.' for x in range(additionSize)]
 	for _ in range(additionSize):
 		newImage.append(deepcopy(emptyLine))
-
 	for line in image:
-		newImage.append(['.' for x in range(additionSize)] + [x for x in line] + ['.' for x in range(additionSize)])
-	
+		newImage.append(['.' for x in range(additionSize)] + [x for x in line] + ['.' for x in range(additionSize)])	
 	for _ in range(additionSize):
 		newImage.append(deepcopy(emptyLine))
 	
 	return newImage
-
 
 def do():
 	strInput = readInputFile(20)
