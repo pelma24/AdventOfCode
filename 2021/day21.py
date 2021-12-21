786ed4678b8e9b2e16a110dfb96291b5b4f27d09
Iris Kellermann <iris.kellermann@methodpark.de>
2021-12-21 10:44:55 +0100

    Remove duplicate options in part2

--- 2021/day21.py
+++ 2021/day21.py
@@ -7,13 +7,8 @@ from collections import Counter, deque
 die = deque([i for i in range(1,101)])
 
 def do1(player1,player2):
-	firstDeque = deque([i for i in range(1,11)])
-	secondDeque = deque([i for i in range(1,11)])
-
-	firstDeque.rotate(-(player1-1))
-	secondDeque.rotate(-(player2-1))
-
-	score1,score2,diceRolls = play1(firstDeque, secondDeque)
+	
+	score1,score2,diceRolls = play1(player1, player2)
 	losingScore = min([score1, score2])
 
 	return diceRolls * losingScore
@@ -23,22 +18,44 @@ def do2(player1,player2):
 	wins1, wins2 = play2(player1, player2, 0, 0)
 
 	print(f'Player1: {wins1}, Player2: {wins2}\n')
-	return max(wins1 / 27, wins2)
+	return max(wins1, wins2)
 
 def getOptions():
-	options1 = []
+	options = []
 	for i in range(1,4):
 		for j in range(1,4):
 			for k in range(1,4):
-				options1.append(i+j+k)
-
-	options = [(x,y) for x in options1 for y in options1]
+				options.append(i+j+k)
 
 	counts = Counter()
 	for option in options:
 		counts[option] += 1
 	
 	return counts
+
+def play1(first, second):
+	score1 = 0
+	score2 = 0
+	diceRolls = 0
+
+	while score1 < 1000 and score2 < 1000:
+		diceNumber = getDice()
+		diceRolls += 3
+
+		first = (first - 1 + diceNumber) % 10 + 1
+		score1 += first
+		if score1 >= 1000:
+			return score1,score2,diceRolls
+
+		diceNumber = getDice()
+		diceRolls += 3
+
+		second = (second - 1 + diceNumber) % 10 + 1
+		score2 += second
+		if score2 >= 1000:
+			return score1,score2,diceRolls
+	
+	return score1,score2,diceRolls
 
 @cache
 def play2(first, second, score1, score2):
@@ -51,47 +68,28 @@ def play2(first, second, score1, score2):
 		result = (0,0)
 		for option,number in counts.items():
 
-			newfirst = (first - 1 + option[0]) % 10 + 1
+			newfirst = (first - 1 + option) % 10 + 1
 			newscore1 = score1 + newfirst
 
-			newsecond = (second - 1 + option[1]) % 10 + 1
-			newscore2 = score2 + newsecond
+			if newscore1 < 21:
+				counts2 = getOptions()
+				for option2,number2 in counts2.items():
+					newsecond = (second - 1 + option2) % 10 + 1
+					newscore2 = score2 + newsecond
 			
-			result1, result2 = play2(newfirst,newsecond,newscore1,newscore2)
-			result = (result[0] + number * result1, result[1] + number * result2)
+					result1, result2 = play2(newfirst,newsecond,newscore1,newscore2)
+					result = (result[0] + number * number2 * result1, result[1] + number * number2 * result2)
+			else:
+				result1, result2 = play2(newfirst,second,newscore1,score2)
+				result = (result[0] + number * result1, result[1] + number * result2)
 		return result
 
-
-def play1(first, second):
-	score1 = 0
-	score2 = 0
-	diceRolls = 0
-
-	while score1 < 1000 and score2 < 1000:
-		diceNumbers =  []
-		for _ in range(3):
-			diceNumbers.append(die[0])
-			die.rotate(-1)
-		diceRolls += 3
-
-		first.rotate(-sum(diceNumbers))
-		score1 += first[0]
-		if score1 >= 1000:
-			return score1,score2,diceRolls
-
-		diceNumbers =  []
-		for _ in range(3):
-			diceNumbers.append(die[0])
-			die.rotate(-1)
-		diceRolls += 3
-
-		second.rotate(-sum(diceNumbers))
-		score2 += second[0]
-	
-	return score1,score2,diceRolls
-
-
-
+def getDice():
+	diceNumbers =  []
+	for _ in range(3):
+		diceNumbers.append(die[0])
+		die.rotate(-1)
+	return sum(diceNumbers)
 
 def do():
 	strInput = readInputFile(21)
