6e1eba7b48b13239a3a51799818a11589c68929f
Iris Kellermann <iris.kellermann@methodpark.de>
2022-01-10 12:16:32 +0100

    Refactor

--- 2021/day24.py
+++ 2021/day24.py
@@ -1,66 +1,19 @@
 from functools import cache
-from HelperFunctions import readInputFile
-from HelperFunctions import readExampleInput
-from HelperFunctions import convertToInt
-from collections import defaultdict
-from copy import deepcopy
 
 inputDict = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10}
 
-def do1(splitInput):
+def do1():
 
 	global inputDict
 	k1 = [1, 1, 1, 26, 1, 1, 1, 26, 26, 1, 26, 26, 26, 26]
-	k2 = [26 for i in range(14)]
 	k3 = [1, 10, 2, 5, 6, 0, 16, 12, 15, 7, 6, 5, 6, 15]
+	special = {3: 10, 7: 11, 8: 7, 10: 13, 11: 0, 12: 11, 13: 0, }
 
 	for position in range(1,14):
 		newDict = inputDict.copy()
 		for oldW, z in inputDict.items():
-			if position == 3:
-				w = (z % 26) - 10
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew
-			elif position == 7:
-				w = (z % 26) - 11
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew
-			elif position == 8:
-				w = (z % 26) - 7
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew
-			elif position == 10:
-				w = (z % 26) - 13
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew	
-			elif position == 11:
-				w = (z % 26)
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew	
-			elif position == 12:
-				w = (z % 26) - 11
-				if w < 1 or w > 9:
-					del newDict[oldW]
-					continue
-				znew = z // 26
-				newDict[oldW * 10 + w] = znew	
-			elif position == 13:
-				w = (z % 26)
+			if position in special.keys():
+				w = (z % 26) - special[position]
 				if w < 1 or w > 9:
 					del newDict[oldW]
 					continue
@@ -68,7 +21,7 @@ def do1(splitInput):
 				newDict[oldW * 10 + w] = znew	
 			else:
 				for w in range(1,10):
-					znew = calculateZ(z, w, k1[position], k2[position], k3[position])
+					znew = calculateZ(z, w, k1[position], k3[position])
 					newDict[oldW * 10 + w] = znew
 			del newDict[oldW]
 		inputDict = newDict.copy()
@@ -78,11 +31,11 @@ def do1(splitInput):
 	return max(possibleValues)			
 
 @cache
-def calculateZ(z, w, k1, k2, k3):
-	z = (z // k1) * k2 + w + k3
+def calculateZ(z, w, k1, k3):
+	z = (z // k1) * 26 + w + k3
 	return z
 
-def do2(splitInput):
+def do2():
 	global inputDict
 	
 	possibleValues = [x for x in inputDict.keys() if inputDict[x] == 0]
@@ -90,12 +43,9 @@ def do2(splitInput):
 	return min(possibleValues)
 
 def do():
-	strInput = readInputFile(24)
-
-	splitInput = strInput.split('\n')
-
-	print(do1(splitInput))
-	print(do2(splitInput))
+	
+	print(do1())
+	print(do2())
 
 	print('done')
 
