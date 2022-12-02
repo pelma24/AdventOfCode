2c7e0c7bea252f585b371511fbde4e8adfbbe48c
Iris Kellermann <iris.kellermann@methodpark.de>
2022-12-02 11:11:45 +0100

    Refactor day2

--- 2022/day2.py
+++ 2022/day2.py
@@ -4,41 +4,47 @@ from HelperFunctions import convertToInt
 
 points = {'X':1, 'Y':2, 'Z':3}
 
-scoreX = {'A':3, 'B':0, 'C':6}
-scoreY = {'A':6, 'B':3, 'C':0}
-scoreZ = {'A':0, 'B':6, 'C':3}
-
-choiceA = {'X':'Z', 'Y':'X', 'Z':'Y'}
-choiceB = {'X':'X', 'Y':'Y', 'Z':'Z'}
-choiceC = {'X':'Y', 'Y':'Z', 'Z':'X'}
-
-
-winScores = {'X':scoreX, 'Y':scoreY, 'Z':scoreZ}
-myChoice = {'A':choiceA, 'B':choiceB, 'C':choiceC}
-
+choices = ['Y', 'A', 'Z', 'B', 'X', 'C', 'Y']
+sameChoice = {'A':'X', 'B':'Y', 'C':'Z'}
 
 def do1(splitInput):
 	score = 0
 	for line in splitInput:
 		opponent,me = line.split(' ')
-		roundScore = points[me] + winScores[me][opponent]
+
+		if [opponent,me] in choices:
+			winScore = 0
+		elif [me,opponent] in choices:
+			winScore = 6
+		else:
+			winScore = 3
+		roundScore = points[me] + winScore
 		score += roundScore
-	
+
 	return score
 
 def do2(splitInput):
 	score = 0
 	for line in splitInput:
 		opponent,result = line.split(' ')
-		me = myChoice[opponent][result]
-		roundScore = points[me] + winScores[me][opponent]
+		opponentIndex = choices.index(opponent)
+		match result:
+			case 'X':
+				me = choices[opponentIndex + 1]
+				winScore = 0
+			case 'Y':
+				me = sameChoice[opponent]
+				winScore = 3
+			case 'Z':
+				me = choices[opponentIndex - 1]
+				winScore = 6
+		roundScore = points[me] + winScore
 		score += roundScore
-	
+
 	return score
 
 def do():
 	strInput = readInputFile(2)
-	#strInput = readExampleInput(2)
 	
 	splitInput = strInput.split('\n')
 	
