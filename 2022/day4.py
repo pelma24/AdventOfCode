89e20986ad862a8cbaec33db388ee5d26ec81350
Iris Kellermann <iris.kellermann@methodpark.de>
2022-12-04 10:43:13 +0100

    Refactor

--- 2022/day4.py
+++ 2022/day4.py
@@ -5,33 +5,29 @@ from HelperFunctions import convertToInt
 def do1(splitInput):
 	fullyContained = 0
 	for pair in splitInput:
-		elves = pair.split(',')
-		assignment = set()
-		for elf in elves:
-			elfStart,elfStop = [int(x) for x in elf.split('-')]
-			elfAssignment = set([x for x in range(elfStart, elfStop + 1)])
-			if not assignment:
-				assignment = elfAssignment
-				continue
-			intersection = elfAssignment.intersection(assignment)
-			if len(intersection) == len(assignment) or len(intersection) == len(elfAssignment):
-				fullyContained += 1
+		elf1Assignment,elf2Assignment = getElvesAssignments(pair)
+		intersection = elf1Assignment.intersection(elf2Assignment)
+		if len(intersection) == len(elf1Assignment) or len(intersection) == len(elf2Assignment):
+			fullyContained += 1
 	return fullyContained
 
 def do2(splitInput):
 	overlaps = 0
 	for pair in splitInput:
-		elves = pair.split(',')
-		assignment = set()
-		for elf in elves:
-			elfStart,elfStop = [int(x) for x in elf.split('-')]
-			elfAssignment = set([x for x in range(elfStart, elfStop + 1)])
-			if not assignment:
-				assignment = elfAssignment
-				continue
-			if elfAssignment.intersection(assignment):
-				overlaps += 1
+		elf1Assignment,elf2Assignment = getElvesAssignments(pair)
+		if elf1Assignment.intersection(elf2Assignment):
+			overlaps += 1
 	return overlaps
+
+def getElvesAssignments(pair):
+	elf1,elf2 = pair.split(',')
+	elf1Start,elf1Stop = [int(x) for x in elf1.split('-')]
+	elf2Start,elf2Stop = [int(x) for x in elf2.split('-')]
+	elf1Assignment = set([x for x in range(elf1Start, elf1Stop + 1)])
+	elf2Assignment = set([x for x in range(elf2Start, elf2Stop + 1)])
+
+	return (elf1Assignment,elf2Assignment)
+
 
 def do():
 	strInput = readInputFile(4)
