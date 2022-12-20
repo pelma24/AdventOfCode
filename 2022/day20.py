1a8b20b0101a90c5c09fcd92639d80444376f2eb
Iris Kellermann <iris.kellermann@methodpark.de>
2022-12-20 14:11:53 +0100

    Refactor

--- 2022/day20.py
+++ 2022/day20.py
@@ -5,45 +5,42 @@ from HelperFunctions import readExampleInput
 from HelperFunctions import convertToInt
 
 def do1(intInput):
-	diff = 100000
-	workingList = deepcopy(intInput)
+	intInput = [(seq,x) for seq,x in enumerate(intInput)]
+	workingList = mix(intInput, 1)
 	length = len(intInput)
-	for number in intInput:
-		currentIndex = workingList.index(number)
-		if number == 0:
-			workingList[currentIndex] = diff
-			continue
-		newIndex = (currentIndex + number) % (length - 1)
-		workingList.pop(currentIndex)
-		workingList.insert(newIndex, number + diff)
-	workingList = [x-diff for x in workingList]
 
-	zeroIndex = workingList.index(0)
+	zeroSeq = [y for (y,x) in intInput if x == 0][0]	
+	zeroIndex = workingList.index((zeroSeq,0))
 	
-	return workingList[(zeroIndex + 1000) % length] + workingList[(zeroIndex + 2000) % length] + workingList[(zeroIndex + 3000) % length]
+	return workingList[(zeroIndex + 1000) % length][1] + workingList[(zeroIndex + 2000) % length][1] + workingList[(zeroIndex + 3000) % length][1]
 
 def do2(intInput):
 	key = 811589153
 	intInput = [(seq, x * key) for seq,x in enumerate(intInput)]
 	length = len(intInput)
-	workingList = deepcopy(intInput)
-	for _ in range(10):
-		for seq,number in intInput:
-			currentIndex = workingList.index((seq,number))
-			if number == 0:
-				zeroSeq = seq
-				continue
-			newIndex = (currentIndex + number) % (length - 1)
-			workingList.pop(currentIndex)
-			workingList.insert(newIndex, (seq,number))
+	workingList = mix(intInput, 10)
 
+	zeroSeq = [y for (y,x) in intInput if x == 0][0]	
 	zeroIndex = workingList.index((zeroSeq,0))
 	
 	return workingList[(zeroIndex + 1000) % length][1] + workingList[(zeroIndex + 2000) % length][1] + workingList[(zeroIndex + 3000) % length][1]
 
+def mix(intInput, times):
+	workingList = deepcopy(intInput)
+	length = len(intInput)
+	for _ in range(times):
+		for seq,number in intInput:
+			currentIndex = workingList.index((seq,number))
+			if number == 0:
+				continue
+			newIndex = (currentIndex + number) % (length - 1)
+			workingList.pop(currentIndex)
+			workingList.insert(newIndex, (seq,number))
+	
+	return workingList
+
 def do():
 	strInput = readInputFile(20)
-	#strInput = readExampleInput(20)
 
 	intInput = convertToInt(strInput.split('\n'))
 
