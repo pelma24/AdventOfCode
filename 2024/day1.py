1a99a58e316748b09747196e901a68d464a78026
Iris Wolf <iris.wolf@ul.com>
2024-12-01 13:15:35 +0100

    Improvements

--- 2024/day1.py
+++ 2024/day1.py
@@ -2,8 +2,8 @@ from HelperFunctions import readInputFile
 from HelperFunctions import readExampleInput
 from HelperFunctions import convertToInt
 
-def do1(splitInput):
-	
+
+def manageLists(splitInput):
     list1 = []
     list2 = []
     for entry in splitInput:
@@ -11,36 +11,32 @@ def do1(splitInput):
         list1.append(entry1)
         list2.append(entry2)
 	
-    list1.sort()
     list1 = convertToInt(list1)
-    list2.sort()
     list2 = convertToInt(list2)
-	
-    result = 0
+    list1.sort()
+    list2.sort()
 
-    for i in range(len(list1)):
-        result = result + abs(list1[i] - list2[i])
+    return (list1, list2)
+
+def do1(splitInput):
+	
+    list1,list2 = manageLists(splitInput)
+	
+    diffList = [abs(x - y) for x,y in zip(list1,list2)]
+    result = sum(diffList)
 
     return result
 
-
 def do2(splitInput):
 	
-    list1 = []
-    list2 = []
-    for entry in splitInput:
-        entry1,entry2 = entry.split('   ')
-        list1.append(entry1)
-        list2.append(entry2)
-    
-    list1 = convertToInt(list1)
-    list2 = convertToInt(list2)
+    list1,list2 = manageLists(splitInput)
 
     result = 0
     for entry in list1:
          result = result + entry * list2.count(entry)
     
     return result
+
 def do():
 	strInput = readInputFile(1)
 
@@ -50,6 +46,5 @@ def do():
 	print(do2(splitInput))
 
 	print('done')
-
 
 do()
